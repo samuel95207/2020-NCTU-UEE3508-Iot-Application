188c77f738271bc93dfd690fefab1176203c4f15
samuelhuang <lspss95207@gmail.com>
2020-12-12 20:21:11 +0800

    Add environment.sh

--- lab3/ROS/environment.sh
+++ lab3/ROS/environment.sh
@@ -1,4 +1,4 @@
-export ROS_MASTER_URI=http://localhost:11411
-export ROS_HOSTNAME=localhost
+export ROS_MASTER_URI=http://192.168.0.102:11311
+export ROS_HOSTNAME=192.168.0.117
 export TURTLEBOT3_MODEL=burger
 source catkin_ws/devel/setup.bash
